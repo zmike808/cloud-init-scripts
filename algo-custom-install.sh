389efc26e9171f1ebc7d2d52bde979818254878b
Michael Zemsky <zmike808@gmail.com>
2019-05-05 12:03:42 +0100

    fixed sysctl config & added sudo

--- algo-custom-install.sh
+++ algo-custom-install.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env sh
-
+#!/bin/sh
+export WINDOWS=true
+export SSH_TUNNELING=true
 curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
 
-iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.10/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
-iptables -t mangle -A FORWARD -m policy --pol ipsec --dir in -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
-iptables -t mangle -A FORWARD -m policy --pol ipsec --dir out -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
-awesomesauce=net.ipv4.ip_no_pmtu_disc=1
+sudo -E iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.10/24 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+sudo -E iptables -t mangle -A FORWARD -m policy --pol ipsec --dir in -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+sudo -E iptables -t mangle -A FORWARD -m policy --pol ipsec --dir out -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+
+sudo -E echo "net.ipv4.ip_no_pmtu_disc=1
 net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr"
+net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 
-echo "$awesomesauce" >> /etc/sysctl.conf
-
-expermentalsauce="net.ipv4.tcp_fastopen=3
+sudo -E echo "net.ipv4.tcp_fastopen=3
 net.core.netdev_max_backlog = 65535
 net.core.optmem_max = 65536
 net.core.somaxconn = 65535
@@ -27,6 +27,4 @@ net.ipv4.tcp_max_tw_buckets = 1048576
 net.ipv4.tcp_no_metrics_save = 0
 net.ipv4.tcp_rfc1337 = 1
 net.ipv4.tcp_slow_start_after_idle = 0
-net.ipv4.tcp_tw_reuse = 1"
-
-echo "$expermentalsauce" >> /etc/sysctl.conf
+net.ipv4.tcp_tw_reuse = 1"  >> /etc/sysctl.conf
